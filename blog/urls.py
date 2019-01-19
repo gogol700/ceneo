2e8c238d0aa4a42b4e3482c8cc135023ec26821c
fanat12k <lunat12k@gmail.com>
2019-01-19 11:38:35 +0000

    v3

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
